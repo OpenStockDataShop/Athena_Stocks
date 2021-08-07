47612be9adf944e351bcc45310c6eb64bb7d5b29
felinakang <77423925+justinekang@users.noreply.github.com>
2021-08-07 12:57:26 -0700

    analytics redirects to login if not logged in

--- django_project/stock_analytics/urls.py
+++ django_project/stock_analytics/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 from . import views
+from users.Forms import Custom_Login_Form
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', LoginView.as_view(
+        authentication_form=Custom_Login_Form),
+        name='login'),
 ]
