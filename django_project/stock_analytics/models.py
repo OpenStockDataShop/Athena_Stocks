12e887e12f320c73a391239df343e764766336df
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2021-08-09 16:20:41 -0700

    created Class for StockNewsLSTM model

--- django_project/stock_analytics/models.py
+++ django_project/stock_analytics/models.py
@@ -10,3 +10,24 @@ class Stock(models.Model):
         return self.name
 
 # create stock_ticker model here
+class StockNews(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    adjclose = models.FloatField()
+    volume = models.IntegerField()
+    neg = models.FloatField()
+    neu = models.FloatField()
+    pos = models.FloatField()
+    compound = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
