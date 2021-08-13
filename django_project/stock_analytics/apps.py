f66a1f9ae8e8c13b2274b607005738473dd28327
Haeyoon Chang <haeyoon@pdx.edu>
2021-08-13 14:10:39 -0700

    created symbol database upon start

--- django_project/stock_analytics/apps.py
+++ django_project/stock_analytics/apps.py
@@ -1,13 +1,38 @@
 from django.apps import AppConfig
-# from .models import Stock
 
-# idea to execute code when django starts up
-# adopted from the below link: 
-# https://pythonin1minute.com/where-to-put-django-startup-code/
 class StockAnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stock_analytics'
 
-    # def ready(self): 
-    #     # startup code here
-    #     nasdaq_query = 
+    def ready(self):
+        
+        import pandas as pd
+        from .models import Stock
+        import os 
+
+        nasdaq_file_path = os.path.join(os.path.dirname(__file__), 'static/nasdaq.csv')
+        nyse_file_path = os.path.join(os.path.dirname(__file__), 'static/nyse.csv')
+        etf_file_path = os.path.join(os.path.dirname(__file__), 'static/etf.csv')
+
+        nasdaq = pd.read_csv(nasdaq_file_path)
+        nyse = pd.read_csv(nyse_file_path)
+        etf = pd.read_csv(etf_file_path)
+
+        nasdaq_stocks = nasdaq[["Symbol", "Name", "Sector", "Industry"]].values.tolist()
+        for stock in nasdaq_stocks:
+            if not Stock.objects.filter(symbol=stock[0]):
+                s = Stock(symbol=stock[0], name=stock[1], sector=stock[2], industry=[3])
+                s.save()
+
+
+        nyse_stocks = nyse[["Symbol", "Name", "Sector", "Industry"]].values.tolist()
+        for stock in nyse_stocks:
+            if not Stock.objects.filter(symbol=stock[0]):
+                s = Stock(symbol=stock[0], name=stock[1], sector=stock[2], industry=[3])
+                s.save()
+
+        etf_stocks = etf[["SYMBOL", "NAME"]].values.tolist()
+        for etf in etf_stocks:
+            if not Stock.objects.filter(symbol=stock[0]):
+                s = Stock(symbol=etf[0], name=etf[1], sector="ETF", industry="ETF")
+                s.save()
